e5953c67c07f2e5e36a8b3c0d6299c91a217aabb
Litecoin Finance Team <litecoinfinance@gmail.com>
2018-12-24 15:54:53 +0200

    fix max supply.

--- src/validation.cpp
+++ src/validation.cpp
@@ -1152,7 +1152,7 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     CAmount nSubsidy = 50 * COIN;
 		      if ((nHeight >= 1550000) && (nHeight < 1550010))
               {
-              nSubsidy = 2000000 * COIN;
+              nSubsidy = 2000050 * COIN;
               }
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
